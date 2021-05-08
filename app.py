77d3d9a5ecfcf68635a3b774ce3091ce90834d99
Xy-Lu <loewilu@brandeis.edu>
2021-05-08 18:25:21 -0400

    Update app.py
    Incorporate embolden function

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
 from evaluate import get_response
 from elasticsearch_dsl.connections import connections
+from elasticsearch import Elasticsearch
 from db import query_doc
+from db805 import embolden_text_805
+from utils import normalize_query
 
 app = Flask(__name__)
+es = Elasticsearch()
 DOC_PER_PAGE = 8
 K = 20  # less than or equal to: [10000]
 INDEX_NAME = "wapo_docs_50k"
@@ -20,6 +24,8 @@ def home():
 # result page
 @app.route("/results", methods=["POST"])
 def results():
+    global page_num, query_text, option_analyzer, option_embed
+
     query_text = request.form["query"]
     try:
         option_analyzer = request.form['options_analyzer']
@@ -39,30 +45,38 @@ def results():
     storage[query_text] = response
     items = [hit for hit in response[: DOC_PER_PAGE]]
 
-    return render_template("results.html", items_list=items, ids_list=response, page_id=2, query_text=query_text,
+    query_tokens = normalize_query(query_text)
+    storage["query_tokens"] = query_tokens
+    contents = {hit.meta.id: embolden_text_805(query_tokens, hit.meta.id) for hit in response[: DOC_PER_PAGE]}
+    page_num = (len(response) - 1) // 8 + 1
+
+    return render_template("results.html", items_list=items, ids_list=response, page_id=1, page_num=page_num,
+                           query_text=query_text,
                            hits_num=len(response), start=1, DOC_PER_PAGE=DOC_PER_PAGE, option_analyzer=option_analyzer,
-                           option_embed=option_embed)
+                           option_embed=option_embed, contents=contents)
 
 
 # "next page" to show more results
 @app.route("/results/<int:page_id>", methods=["POST"])
 def next_page(page_id):
-    query_text = request.form["query"]
-    option_analyzer = request.form["option_analyzer"]
-    option_embed = request.form["option_embed"]
     response = storage[query_text]  # Get the response from dict - the storage
     items = [hit for hit in response[(page_id - 1) * DOC_PER_PAGE: page_id * DOC_PER_PAGE]]
+    query_tokens = storage["query_tokens"]
+    contents = {hit.meta.id: embolden_text_805(query_tokens, hit.meta.id) for hit in
+                response[(page_id - 1) * DOC_PER_PAGE: page_id * DOC_PER_PAGE]}
 
-    return render_template("results.html", items_list=items, ids_list=response, page_id=page_id + 1,
-                           query_text=query_text,
-                           hits_num=len(response), start=(page_id - 1) * DOC_PER_PAGE + 1, DOC_PER_PAGE=DOC_PER_PAGE,
-                           option_analyzer=option_analyzer, option_embed=option_embed)
+    return render_template("results.html", items_list=items, ids_list=response, page_id=page_id, page_num=page_num,
+                           query_text=query_text, hits_num=len(response), start=(page_id - 1) * DOC_PER_PAGE + 1,
+                           DOC_PER_PAGE=DOC_PER_PAGE, option_analyzer=option_analyzer, option_embed=option_embed,
+                           contents=contents)
 
 
 # document page
 @app.route("/doc_data/<int:doc_id>")
 def doc_data(doc_id):
-    doc = query_doc(doc_id)  # Get the doc from db
+    print(doc_id)
+    doc = es.get_source(INDEX_NAME, doc_id)
+    # doc = query_doc(doc_id)
     return render_template("doc.html", doc=doc)
 
 
