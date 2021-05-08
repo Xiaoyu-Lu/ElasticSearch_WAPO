d29824a89140b92e4c2a7a59eda7662dadefdb8c
Xy-Lu <loewilu@brandeis.edu>
2021-05-08 18:28:28 -0400

    Update utils.py
    Add helper functions for embolden functions

--- utils.py
+++ utils.py
@@ -1,12 +1,24 @@
-from typing import Dict, Union, List, Generator
+from typing import Dict, Union, List, Generator, DefaultDict, Set, Tuple, Any
 import os
 import json
 import xml.etree.ElementTree as ET
 from collections import defaultdict
+import nltk
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+import bisect
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+from hyphenate import hyphenate_word
+import string
+
+THRES = 40
+PUNC = set(string.punctuation)
+lemmatizer = WordNetLemmatizer()
+detokenizer = TreebankWordDetokenizer()
 
 
 def load_clean_wapo_with_embedding(
-    wapo_jl_path: Union[str, os.PathLike]
+        wapo_jl_path: Union[str, os.PathLike]
 ) -> Generator[Dict, None, None]:
     """
     load wapo docs as a generator
@@ -39,6 +51,88 @@ def parse_wapo_topics(xml_file: str) -> Dict[str, List[str]]:
                     # append others to this topic id
                     topic_mapping[topic_id].append(child.text.strip().split("\n")[-1])
     return topic_mapping
+
+
+def get_wordnet_pos(word: str) -> Dict[str, Any]:
+    """
+    Map POS tag to first character lemmatize() accepts
+    reference: https://www.machinelearningplus.com/nlp/lemmatization-examples-python/
+    """
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
+
+
+def normalize_func(text: str) -> List[str]:
+    """telling -> tell, are-> be"""
+    tokens = nltk.word_tokenize(text)  # need to be consistent with the basic tokenize used in other functions
+    return [lemmatizer.lemmatize(w.lower(), get_wordnet_pos(w.lower())) for w in tokens]
+
+
+def normalize_query(text: str) -> List[str]:
+    """telling -> tell, are-> be, remove punctuation"""
+    tokens = nltk.word_tokenize(text.lower())
+    return [lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in tokens if w not in PUNC]
+
+
+def get_word_dict(doc: str) -> DefaultDict[str, Set[int]]:
+    # doc_tokens_basic = nltk.word_tokenize(doc)
+    doc_tokens = normalize_func(doc)
+    # assert len(doc_tokens) == len(doc_tokens_basic)
+    word_idx_dict = defaultdict(set)
+    for i, token in enumerate(doc_tokens):
+        if token not in PUNC and len(token) > 4:
+            split_words: List[str] = hyphenate_word(token)
+            for word in split_words:
+                if len(word) >= 4:
+                    word_idx_dict[word].add(i)  # hyphenate_word can help handle fish vs. Frankenfish
+        if token in [".", "?", "!"]:
+            word_idx_dict["eos_sign"].add(i)
+        else:
+            word_idx_dict[token].add(i)  # cannot handle fish vs. Frankenfish alone
+    return word_idx_dict
+
+
+def get_seg(seg_idx: List[int], breakpoints: List[int]) -> Tuple[Union[int, Any], Union[int, Any], int, int]:
+    """
+    get the subset with maximum length
+    Example1:
+    seg_idx =  [4, 10, 19]
+    breakpoints = [2,3,5,9,11,13,15,18]
+    res: 4, 8 => [11,13,15,18]
+    Example2:
+    seg_idx = [32, 60, 88, 118, 134, 144, 196, 204, 233, 265, 279, 313]
+    breakpoints = [6, 8, 65, 253]
+    res: 0, 2 => [6,8]
+    """
+    l, r = 0, 0
+    left, right = 0, 0
+    bos_i, eos_i = 0, 0
+    n = len(breakpoints)
+    i, flag = 0, 0
+    for j, idx in enumerate(seg_idx):
+        i = bisect.bisect(breakpoints, idx, lo=i)
+        if i >= n - 1 and flag:  # optimize: reach the end of break-point, no needs to keep counting
+            break
+        elif i >= n - 1 and not flag:
+            flag += 1
+        if i - r > right - left:
+            left, right = r, i
+            bos_i, eos_i = j - 1, j
+        if i > r:
+            l, r = r, i
+    return left, right, bos_i, eos_i
+
+
+def add_bold(sent_pieces: List[str], bold_indices: List[int]) -> str:
+    """Untokenize a sentence with bold tags"""
+    for idx in bold_indices[::-1]:
+        sent_pieces[idx] = "<b>" + sent_pieces[idx] + "</b>"
+    return detokenizer.detokenize(sent_pieces)
 
 
 if __name__ == "__main__":
