c5d7b5ed579cf99bc7133b2e0327f774d84cebdc
Xy-Lu <loewilu@brandeis.edu>
2021-05-12 00:55:59 -0400

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -10,8 +10,8 @@
 ## load wapo docs into the index called "wapo_docs_50k_lf"
 #python load_es_index.py --index_name wapo_docs_50k_lf --wapo_path data/subset_wapo_50k_sbert_ft_lf_filtered.jl
 
-echo "----------------Evalutation 805----------------"
 TOPIC_ID=805
+echo "----------------Evalutation $TOPIC_ID----------------"
 python evaluate.py --index_name wapo_docs_50k --topic_id $TOPIC_ID --query_type title --top_k 20
 python evaluate.py --index_name wapo_docs_50k --topic_id $TOPIC_ID --query_type title --vector_name ft_vector --top_k 20
 python evaluate.py --index_name wapo_docs_50k --topic_id $TOPIC_ID --query_type title --vector_name sbert_vector --top_k 20
